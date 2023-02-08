262771be39df72394a8e7c2cdeddb51f63157e0f
Bibek KC <bibekyess@gmail.com>
2023-02-08 16:32:37 +0900

    Adds pyspellchecker

---
+++ spell_check.py
@@ -0,0 +1,22 @@
+import spellchecker
+
+
+def correct_typos(sentence):
+    # Initialize the spell checker object
+    spell = spellchecker.SpellChecker(language="en")
+    # Adds Bibek to its frequency dictionary to make it a known word
+    spell.word_frequency.load_words(["Bibek", "Bibek's"])
+    sentence_split = sentence.split()
+    # Find the typos in the input sentence
+    typos = spell.unknown(sentence_split)
+    # Correct the typos
+    corrected_sentence = [
+        spell.correction(word)
+        if spell.correction(word)
+        else word
+        if word in typos
+        else word
+        for word in sentence_split
+    ]
+    # Return the corrected sentence as a string
+    return " ".join(corrected_sentence)
